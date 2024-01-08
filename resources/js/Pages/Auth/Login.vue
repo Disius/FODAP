6002196b76f67fdc4dfec1e391aecd658b7b4e58
Daniel Fernández <defp_99@hotmail.com>
2024-01-08 14:02:07 -0600

    08/01/2024

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -147,5 +147,10 @@ onMounted(() => {
                 </Link>
             </div>
         </template>
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="flex justify-center">
+                <v-btn>Iniciar sesión con Microsoft</v-btn>
+            </div>
+        </div>
     </GuestLayout>
 </template>
