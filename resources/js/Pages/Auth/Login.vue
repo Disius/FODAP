d627386214adebadd8da2da884ffeb3ba4629fd6
Daniel Fernández <defp_99@hotmail.com>
2024-01-18 22:45:51 -0600

    microsoft 12

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -147,7 +147,7 @@ onMounted(() => {
                 </Link>
             </div>
         </template>
-        <Link :href=route('connect') as="button">
+        <Link :href="route('connect')" as="button">
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 <div class="flex justify-center">
                     <v-btn append-icon="mid-microsoft">Iniciar sesión con Microsoft</v-btn>
