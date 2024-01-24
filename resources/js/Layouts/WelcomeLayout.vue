49ab78afcdec03db8d8de4e8e5b1bb9846bcf64c
Daniel Fernández <defp_99@hotmail.com>
2024-01-23 22:32:00 -0600

    changes 8

--- resources/js/Layouts/WelcomeLayout.vue
+++ resources/js/Layouts/WelcomeLayout.vue
@@ -12,7 +12,7 @@ import ApplicationLogoEducacion from "@/Components/ApplicationLogoEducacion.vue"
                 <div class="flex items-center space-x-4">
                     <Link :href="route('main')">
                         <ApplicationLogo
-                            class="block h-32 w-36 fill-current text-gray-800 mb-5"
+                            class="block h-40 w-36 fill-current text-gray-800 mb-5"
                         />
                     </Link>
                 </div>
@@ -32,7 +32,7 @@ import ApplicationLogoEducacion from "@/Components/ApplicationLogoEducacion.vue"
             </main>
         </div>
 
-        <footer class=" bg-white shadow-md py-6">
+        <footer class=" bg-blue-600 shadow-md py-6">
             <div class="container mx-auto text-center">
                 <div class="flex justify-center items-center space-x-4">
                     <ApplicationLogoEducacion
