1012fbe50ae967214be81b089560ca824a3c81a2
Daniel Fernández <defp_99@hotmail.com>
2024-01-23 22:29:34 -0600

    changes 7

--- resources/js/Layouts/WelcomeLayout.vue
+++ resources/js/Layouts/WelcomeLayout.vue
@@ -12,7 +12,7 @@ import ApplicationLogoEducacion from "@/Components/ApplicationLogoEducacion.vue"
                 <div class="flex items-center space-x-4">
                     <Link :href="route('main')">
                         <ApplicationLogo
-                            class="block h-28 w-36 fill-current text-gray-800 mb-5"
+                            class="block h-32 w-36 fill-current text-gray-800 mb-5"
                         />
                     </Link>
                 </div>
