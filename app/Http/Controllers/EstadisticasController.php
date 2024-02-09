e2b2ad70c20e05f191ce3b6258122245571e3452
Daniel <defp_99@hotmail.com>
2024-02-09 10:06:03 -0600

    estadisticas v3

--- app/Http/Controllers/EstadisticasController.php
+++ app/Http/Controllers/EstadisticasController.php
@@ -11,7 +11,8 @@ class EstadisticasController extends Controller
     public function index_estadisticas(){
         return Inertia::render('Views/desarrollo/estadisticas/Estadisticas', [
             'cursos_anio' => $this->estadisticas_de_curso_por_anio(),
-            'cursos_periodos' => $this->estadistica_cursos_periodo()
+            'cursos_periodos' => $this->estadistica_cursos_periodo(),
+            'cursos_tipo' => $this->estadistica_cursos_tipo(),
         ]);
     }
 
@@ -37,5 +38,40 @@ class EstadisticasController extends Controller
             })->count();
 
         return [$cursos_enero_junio, $cursos_agosto_diciembre];
+    }
+    public static function estadistica_cursos_tipo(){
+        $anio = date('Y');
+        $cursos_taller = DeteccionNecesidades::with('inscritos') ->whereYear('fecha_F', '=', $anio)
+            ->where(function($query){
+                $query->where('estado', '=', 2)
+                    ->where('tipo_actividad', '=', 1);
+            })->count();
+        $cursos_curso = DeteccionNecesidades::with('inscritos') ->whereYear('fecha_F', '=', $anio)
+            ->where(function($query){
+                $query->where('estado', '=', 2)
+                    ->where('tipo_actividad', '=', 2);
+            })->count();
+        $cursos_curso_taller = DeteccionNecesidades::with('inscritos') ->whereYear('fecha_F', '=', $anio)
+            ->where(function($query){
+                $query->where('estado', '=', 2)
+                    ->where('tipo_actividad', '=', 3);
+            })->count();
+        $cursos_foro = DeteccionNecesidades::with('inscritos') ->whereYear('fecha_F', '=', $anio)
+            ->where(function($query){
+                $query->where('estado', '=', 2)
+                    ->where('tipo_actividad', '=', 4);
+            })->count();
+        $cursos_seminario = DeteccionNecesidades::with('inscritos') ->whereYear('fecha_F', '=', $anio)
+            ->where(function($query){
+                $query->where('estado', '=', 2)
+                    ->where('tipo_actividad', '=', 5);
+            })->count();
+        $cursos_diplomado = DeteccionNecesidades::with('inscritos') ->whereYear('fecha_F', '=', $anio)
+            ->where(function($query){
+                $query->where('estado', '=', 2)
+                    ->where('tipo_actividad', '=', 6);
+            })->count();
+
+        return [$cursos_taller, $cursos_curso, $cursos_curso_taller, $cursos_foro, $cursos_seminario, $cursos_diplomado];
     }
 }
--- resources/js/Pages/Views/desarrollo/estadisticas/Estadisticas.vue
+++ resources/js/Pages/Views/desarrollo/estadisticas/Estadisticas.vue
@@ -2,10 +2,13 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import GraficaCursos from "@/Pages/Views/desarrollo/estadisticas/components/GraficaCursos.vue";
 import {computed, ref} from "vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 const props = defineProps({
     cursos_anio: Number,
     cursos_periodos: Array,
+    cursos_tipo: Array,
 })
 
 const date = computed(() => {
@@ -25,14 +28,19 @@ console.log(props.cursos_periodos)
         <div>
             <div class="mt-2 mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 mt-2 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <div class="flex justify-center">
-                        <h2 class="text-6xl font-semibold text-gray-900">Cursos</h2>
+                    <div class="grid grid-cols-1 lg:grid-cols-2">
+                        <div class="flex justify-center">
+                            <h2 class="text-6xl font-semibold text-gray-900">Cursos</h2>
+                        </div>
+                        <div class="flex justify-center">
+                            <primary-button>Descargar Reporte</primary-button>
+                        </div>
                     </div>
                     <div class="grid grid-cols-1 gap-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
                         <div class="flex justify-center">
                             <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
                                 <div class="flex justify-center">
-                                    <h2 class="text-lg font-medium text-gray-900">Total de cursos realizados en {{date}}</h2>
+                                    <h2 class="text-sm font-medium text-gray-900">Total de cursos realizados en {{date}}</h2>
                                 </div>
                                 <div class="container mx-auto">
                                     <p class="font-semibold pa-5 text-center text-6xl mt-5">
@@ -44,7 +52,7 @@ console.log(props.cursos_periodos)
                         <div class="flex justify-center">
                             <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
                                 <div class="flex justify-center">
-                                    <h2 class="text-lg font-medium text-gray-900">Total de cursos realizados en el periodo enero-junio de {{date}}</h2>
+                                    <h2 class="text-sm font-medium text-gray-900">Total de cursos realizados en el periodo enero-junio de {{date}}</h2>
                                 </div>
                                 <div class="container mx-auto mt-7">
                                     <p class="font-semibold pa-5 text-center text-6xl">
@@ -56,7 +64,7 @@ console.log(props.cursos_periodos)
                         <div class="flex justify-center">
                             <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
                                 <div class="flex justify-center">
-                                    <h2 class="text-lg font-medium text-gray-900">Total de cursos realizados en el periodo agosto-diciembre de {{date}}</h2>
+                                    <h2 class="text-sm font-medium text-gray-900">Total de cursos realizados en el periodo agosto-diciembre de {{date}}</h2>
                                 </div>
                                 <div class="container mx-auto">
                                     <p class="font-semibold pa-5 text-center text-6xl">
@@ -65,6 +73,90 @@ console.log(props.cursos_periodos)
                                 </div>
                             </div>
                         </div>
+                        <div class="flex justify-center">
+                            <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
+                                <div class="flex justify-center">
+                                    <h2 class="text-sm font-medium text-gray-900">Total de cursos realizados en el periodo agosto-diciembre de {{date}}</h2>
+                                </div>
+                                <div class="container mx-auto">
+                                    <p class="font-semibold pa-5 text-center text-6xl">
+                                        {{props.cursos_periodos[1]}}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex justify-center">
+                            <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
+                                <div class="flex justify-center">
+                                    <h2 class="text-sm font-medium text-gray-900">Total de TALLERES realizados en {{date}}</h2>
+                                </div>
+                                <div class="container mx-auto">
+                                    <p class="font-semibold pa-5 text-center text-6xl">
+                                        {{props.cursos_tipo[0]}}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex justify-center">
+                            <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
+                                <div class="flex justify-center">
+                                    <h2 class="text-sm font-medium text-gray-900">Totalidad de actividades tipo curso realizados en {{date}}</h2>
+                                </div>
+                                <div class="container mx-auto">
+                                    <p class="font-semibold pa-5 text-center text-6xl">
+                                        {{props.cursos_tipo[1]}}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex justify-center">
+                            <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
+                                <div class="flex justify-center">
+                                    <h2 class="text-sm font-medium text-gray-900">Total de CURSOS-TALLER realizados en {{date}}</h2>
+                                </div>
+                                <div class="container mx-auto">
+                                    <p class="font-semibold pa-5 text-center text-6xl">
+                                        {{props.cursos_tipo[2]}}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="flex justify-center">
+                                <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
+                                    <div class="flex justify-center">
+                                        <h2 class="text-sm font-medium text-gray-900">Total de FOROS realizados en {{date}}</h2>
+                                    </div>
+                                    <div class="container mx-auto">
+                                        <p class="font-semibold pa-5 text-center text-6xl">
+                                            {{props.cursos_tipo[3]}}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex justify-center">
+                                <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
+                                    <div class="flex justify-center">
+                                        <h2 class="text-sm font-medium text-gray-900">Totalidad de SEMINARIOS realizados en {{date}}</h2>
+                                    </div>
+                                    <div class="container mx-auto">
+                                        <p class="font-semibold pa-5 text-center text-6xl">
+                                            {{props.cursos_tipo[4]}}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="flex justify-center">
+                                <div class="bg-gradient-to-b from-white to-blue-200 p-4 mt-2 sm:p-8 shadow sm:rounded-lg">
+                                    <div class="flex justify-center">
+                                        <h2 class="text-sm font-medium text-gray-900">Total de DIPLOMADOS realizados en {{date}}</h2>
+                                    </div>
+                                    <div class="container mx-auto">
+                                        <p class="font-semibold pa-5 text-center text-6xl">
+                                            {{props.cursos_tipo[5]}}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
                 </div>
             </div>
