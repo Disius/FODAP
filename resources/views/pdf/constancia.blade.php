1535ad75f5db83143b69a345bb46e6d2677827dd
Daniel <defp_99@hotmail.com>
2024-03-05 16:04:02 -0600

    Constancia test10

--- resources/views/pdf/constancia.blade.php
+++ resources/views/pdf/constancia.blade.php
@@ -46,7 +46,7 @@
             justify-content: center;
             align-items: center;
             text-align: start;
-            margin-left: 72px;
+            margin-left: 73px;
             font-family: 'Montserrat-Black';
             font-size: 14pt;
             font-weight: bold;
