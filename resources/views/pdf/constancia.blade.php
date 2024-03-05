20e93e9e0a5250cf5ce84e203466dc6f85f0c716
Daniel <defp_99@hotmail.com>
2024-03-05 15:06:48 -0600

    Constancia test10

--- resources/views/pdf/constancia.blade.php
+++ resources/views/pdf/constancia.blade.php
@@ -28,7 +28,7 @@
         .header {
             display: flex;
             position: relative;
-            top: 165px;
+            top: 167px;
             flex-direction: column;
             justify-content: center;
             align-items: center;
@@ -41,7 +41,7 @@
         .text-position {
             display: flex;
             position: relative;
-            top: 173px;
+            top: 174px;
             flex-direction: column;
             justify-content: center;
             align-items: center;
