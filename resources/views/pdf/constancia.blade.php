dfc02513400809a91145b1583c6fb051aa199e49
Daniel <defp_99@hotmail.com>
2024-03-06 14:39:26 -0600

    Data

--- resources/views/pdf/constancia.blade.php
+++ resources/views/pdf/constancia.blade.php
@@ -47,7 +47,7 @@
             justify-content: center;
             align-items: center;
             text-align: start;
-            margin-left: 91px;
+            margin-left: 90px;
             font-family: 'Montserrat-Black', sans-serif;
             font-size: 14pt;
             font-weight: bold;
